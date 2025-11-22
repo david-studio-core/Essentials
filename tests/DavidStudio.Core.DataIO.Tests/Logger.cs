11e9b1451f1b9d11e8b24be9dd322d191c7d37cb
davidkhachatryan123 <davidkhachatryan359@gmail.com>
2025-11-23 02:31:29 +0400

    chore: clean-up tests

--- tests/DavidStudio.Core.DataIO.Tests/Logger.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.Extensions.Logging;
-
-namespace DavidStudio.Core.DataIO.Tests;
-
-public static class Logger
-{
-    public static readonly ILoggerFactory TestsLoggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
-}
